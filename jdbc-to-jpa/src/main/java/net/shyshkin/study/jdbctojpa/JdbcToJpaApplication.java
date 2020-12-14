ce42717922aa382d5a53088cf31341a2f5f2e479
Art <d.art.shishkin@gmail.com>
2020-12-14 02:11:08 +0200

    Initial commit

---
+++ jdbc-to-jpa/src/main/java/net/shyshkin/study/jdbctojpa/JdbcToJpaApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.jdbctojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcToJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcToJpaApplication.class, args);
+    }
+
+}
---
+++ jdbc-to-jpa/src/test/java/net/shyshkin/study/jdbctojpa/JdbcToJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.jdbctojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcToJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
