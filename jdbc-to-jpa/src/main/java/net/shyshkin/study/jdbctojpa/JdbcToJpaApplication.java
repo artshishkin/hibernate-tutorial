d7800288ea584e89d1acc2154446cb9deef6fc0b
Art <d.art.shishkin@gmail.com>
2020-12-14 03:20:05 +0200

    28. Step 06 - Execute the findAll method using CommandLineRunner (#1)

--- jdbc-to-jpa/src/main/java/net/shyshkin/study/jdbctojpa/JdbcToJpaApplication.java
+++ jdbc-to-jpa/src/main/java/net/shyshkin/study/jdbctojpa/JdbcToJpaApplication.java
@@ -1,13 +1,25 @@
 package net.shyshkin.study.jdbctojpa;
 
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.jdbctojpa.dao.PersonJdbcDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class JdbcToJpaApplication {
+public class JdbcToJpaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JdbcToJpaApplication.class, args);
     }
 
+    @Autowired
+    PersonJdbcDao personJdbcDao;
+
+    @Override
+    public void run(String... args) throws Exception {
+        personJdbcDao.findAll().forEach(person -> log.debug("{}", person));
+    }
 }
