84f5f7930b9acfd64f07dfd6b5d6539785f096c7
Art <d.art.shishkin@gmail.com>
2020-12-14 02:48:34 +0200

    26. Step 04 - Populate data into Person Table (#1)

--- jdbc-to-jpa/src/main/resources/data.sql
+++ jdbc-to-jpa/src/main/resources/data.sql
@@ -1,8 +1,14 @@
 create table person
 (
-    id integer not null,
-    name varchar(255),
-    location varchar(255),
+    id         integer not null,
+    name       varchar(255),
+    location   varchar(255),
     birth_date timestamp,
     primary key (id)
 );
+
+insert into person (id, name, location, birth_date)
+VALUES (10001, 'Art', 'Kram', sysdate);
+
+insert into person (id, name, location, birth_date)
+VALUES (10002, 'Kate', 'Kram', current_timestamp);
