e40552bb238ff7a58e9e9c285c20db8f3111e56b
Art <d.art.shishkin@gmail.com>
2020-12-16 01:56:49 +0200

    36. Step 14 - Defining Person Entity (#1)

--- jdbc-to-jpa/src/main/java/net/shyshkin/study/jdbctojpa/domain/Person.java
+++ jdbc-to-jpa/src/main/java/net/shyshkin/study/jdbctojpa/domain/Person.java
@@ -5,14 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     private String name;
     private String location;
