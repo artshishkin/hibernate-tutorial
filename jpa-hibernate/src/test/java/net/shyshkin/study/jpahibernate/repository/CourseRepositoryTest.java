bbd7d3ed2a87ab07bac1b7ce1d258301321d000c
Art <d.art.shishkin@gmail.com>
2020-12-18 21:42:43 +0200

    52. Step 04 - Configuring application.properties to enable H2 console and logging (#2)

--- jpa-hibernate/src/test/java/net/shyshkin/study/jpahibernate/repository/CourseRepositoryTest.java
+++ jpa-hibernate/src/test/java/net/shyshkin/study/jpahibernate/repository/CourseRepositoryTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ComponentScan
+@TestPropertySource(properties = {"/application.properties"})
 class CourseRepositoryTest {
 
     @Autowired
