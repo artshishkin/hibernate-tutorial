4d4aec25f758fc7a75d0069ccf074653bdb4e760
Art <d.art.shishkin@gmail.com>
2020-12-19 22:59:15 +0200

    65. Step 16 - JPA and Hibernate Annotations - @Table (#2)

--- jpa-hibernate/src/main/resources/data.sql
+++ jpa-hibernate/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into course (id, name)
+insert into course_details (id, name)
 values (10001,'Spring Boot');
 
-insert into course (id, name)
+insert into course_details (id, name)
 values (10002,'Hibernate');
