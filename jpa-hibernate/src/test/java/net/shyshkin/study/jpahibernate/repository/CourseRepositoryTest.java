6890d385a45dec5b366b993a5067fd1326746dcf
Art <d.art.shishkin@gmail.com>
2020-12-19 21:46:36 +0200

    Modified Tests for clear and detach (#2)

--- jpa-hibernate/src/test/java/net/shyshkin/study/jpahibernate/repository/CourseRepositoryTest.java
+++ jpa-hibernate/src/test/java/net/shyshkin/study/jpahibernate/repository/CourseRepositoryTest.java
@@ -1,13 +1,18 @@
 package net.shyshkin.study.jpahibernate.repository;
 
 import net.shyshkin.study.jpahibernate.entity.Course;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 @ComponentScan
@@ -85,14 +90,35 @@ class CourseRepositoryTest {
     }
 
     @Test
+    @DisplayName("Detaching removes the given entity from the persistence context, causing a managed entity to become detached. Unflushed changes made to the entity if any (including removal of the entity), will not be synchronized to the database. Entities which previously referenced the detached entity will continue to reference it.")
     void playWithEntityManagerDetach() {
         courseRepository.playWithEntityManagerDetach();
         syncDB();
+        assertAll(
+                () -> assertThat(countCoursesByName("Play course 1")).isEqualTo(0L),
+                () -> assertThat(countCoursesByName("Play course 2")).isEqualTo(1L),
+                () -> assertThat(countCoursesByName("Updated course 1")).isEqualTo(1L),
+                () -> assertThat(countCoursesByName("Updated course 2")).isEqualTo(0L)
+        );
     }
 
     @Test
+    @DisplayName("After Clearing the persistence context all managed entities become detached. Changes made to entities that have not been flushed to the database will not be persisted.")
     void playWithEntityManagerClear() {
         courseRepository.playWithEntityManagerClear();
         syncDB();
+        assertAll(
+                () -> assertThat(countCoursesByName("Play course 1")).isEqualTo(1L),
+                () -> assertThat(countCoursesByName("Play course 2")).isEqualTo(1L),
+                () -> assertThat(countCoursesByName("Updated course 1")).isEqualTo(0L),
+                () -> assertThat(countCoursesByName("Updated course 2")).isEqualTo(0L)
+        );
+    }
+
+    private Long countCoursesByName(String name) {
+        EntityManager entityManager = testEntityManager.getEntityManager();
+        TypedQuery<Long> typedQuery = entityManager.createQuery("select count(c) from Course c where c.name = '" + name + "'", Long.class);
+
+        return typedQuery.getSingleResult();
     }
 }
