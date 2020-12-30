1b3bee24be5f244a18b5a2d301e5c70c317f28ba
Art <d.art.shishkin@gmail.com>
2020-12-30 17:16:20 +0200

    120. Step 66 - Spring Data JPA Repository - CRUD Methods (#9)

--- jpa-hibernate/src/test/java/net/shyshkin/study/jpahibernate/repository/CourseSpringDataRepositoryTest.java
+++ jpa-hibernate/src/test/java/net/shyshkin/study/jpahibernate/repository/CourseSpringDataRepositoryTest.java
@@ -1,11 +1,14 @@
 package net.shyshkin.study.jpahibernate.repository;
 
 import net.shyshkin.study.jpahibernate.entity.Course;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Example;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,6 +47,41 @@ class CourseSpringDataRepositoryTest {
 
         //then
         assertThat(courseOptional.isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Test is in a transaction so second courseRepository.save is not necessary")
+    void saveAndUpdate() {
+        //given
+        Course course = new Course("Microservices in 50 Steps");
+
+        //when
+        courseRepository.save(course);
+
+        course.setName("Micro 2021");
+//        courseRepository.save(course);
+
+        //then
+        long count = courseRepository.count(Example.of(new Course("Micro 2021")));
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    void findAll() {
+        //when
+        List<Course> courseList = courseRepository.findAll();
+
+        //then
+        assertThat(courseList).hasSizeGreaterThan(2);
+    }
+
+    @Test
+    void count() {
+        //when
+        long count = courseRepository.count();
+
+        //then
+        assertThat(count).isGreaterThan(2);
     }
 
     private void syncDB() {
